a3914551c1dddec44d5f0c17c7e88bead2da7de2
EianX <30997809+EianX@users.noreply.github.com>
2020-11-08 03:41:39 +0200

    menus

--- prototype/prototype/prototype.ino
+++ prototype/prototype/prototype.ino
@@ -1,8 +1,10 @@
 //Including required libraries
 #include <LiquidCrystal.h>
 #include <max6675.h>
+#include <Encoder.h>
 //Pins variables
-const int encoder[3] = {,,};
+Encoder myEnc(, );
+const int encButton = ;
 const int lamp[3] = { , , };
 const int heatRelay;
 const int irPin[4]={,,,};
@@ -13,7 +15,8 @@ const int fan;
 const int vacuum;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 //Data variables
-int matrial = 0; //Acrylic-PVC:0, PP:1
+int material = 0;//Acrylic-PVC:0, PP:1
+String materialName = "";
 int thick = 0;
 float temp =0.0;
 float tempMax=0.0,tempMin=0.0;
@@ -24,27 +27,223 @@ int sheetCount = 0;
 int vacuumTime = 0;
 int irRead[4]={,,,};
 //LCD Menues variables
-int counter = 0;
+long counter = 0;
 int menu = 0;
 int opt = 0;
-int last_counter = 0;
-bool clk_State;
-bool Last_State; 
-bool dt_State;
+long lastCounter = -999;
+bool pushed = true;
 //Material and thickness heat forming data
 const float tempRange[2][2] = {{140.0,190.0},{150.0,180.0}};
-const int thickTime[2][6]={{0,30,60,90,120,45},{0,50,100,150,200,75}}; //Index = thick except 1.5mm thickness is index 5
+const int thickTime[2][4]={{0,30,60,90},{0,50,100,150}}; //Index = thickness
 //LCD special characters
-uint8_t arrow[8] = {0x0, 0x04 ,0x06, 0x1f, 0x06, 0x04, 0x00, 0x00};
+uint8_t arrow[8] = {0x0, 0x04, 0x06, 0x1f, 0x06, 0x04, 0x00, 0x00};
+uint8_t checked[8] = {0x1F, 0x13, 0x13, 0x13, 0x1B, 0x1B, 0x15, 0x1F};
+
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(encButton, INPUT);
+  
   lcd.begin(20, 4);
+  lcd.init();                 //Init the LCD
+  lcd.backlight();            //Activate backlight
+  lcd.createChar(0, arrow);   //Create the arrow symbol
+  lcd.createChar(1, checked);
+  lcd.home();                 //Home the LCD
+
+  lcd.clear();
+  lcd.setCursor(3, 0);  
+  lcd.print("Vacuum Forming");
+  lcd.setCursor(2, 1);  
+  lcd.print("Machine Prototype");
+  lcd.setCursor(5, 3);  
+  lcd.print("WELCOME :D");
+  delay(2500);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  counter = myEnc.read();
+  
+  if(counter > 17){
+    counter = 17;
+  }
+  if(counter < 0){
+    counter = 0;
+  }
 
+  if(counter != lastCounter || pushed){
+    pushed = false;
+    
+    if(menu == 0)
+    {  
+      if(0 <= counter && counter < 6)
+      {
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Select material:");
+        lcd.setCursor(0,1);
+        lcd.write(0);  
+        lcd.print("PVC");
+        lcd.setCursor(0,2);  
+        lcd.print(" Acrylic");
+        lcd.setCursor(0,3);  
+        lcd.print(" PP");
+        opt = 1;
+      }
+      if(6 <= counter && counter < 12)
+      {
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Select material:");
+        lcd.setCursor(0,1);  
+        lcd.print(" PVC");
+        lcd.setCursor(0,2); 
+        lcd.write(0); 
+        lcd.print("Acrylic");
+        lcd.setCursor(0,3);  
+        lcd.print(" PP");
+        opt = 2;
+      }
+      if(12 <= counter && counter < 18)
+      {
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Select material:");
+        lcd.setCursor(0,1);  
+        lcd.print(" PVC");
+        lcd.setCursor(0,2);  
+        lcd.print(" Acrylic");
+        lcd.setCursor(0,3); 
+        lcd.write(0); 
+        lcd.print("PP");
+        opt = 3;
+      }
+    }
+    if(menu == 1)
+    {  
+      if(0 <= counter && counter < 6)
+      {
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Select thickness:");
+        lcd.setCursor(0,1);
+        lcd.write(0);  
+        lcd.print("1 mm");
+        lcd.setCursor(0,2);  
+        lcd.print(" 2 mm");
+        lcd.setCursor(0,3);  
+        lcd.print(" 3 mm");
+        opt = 1;
+      }
+      if(6 <= counter && counter < 12)
+      {
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Select thickness:");
+        lcd.setCursor(0,1);  
+        lcd.print(" 1 mm");
+        lcd.setCursor(0,2); 
+        lcd.write(0); 
+        lcd.print("2 mm");
+        lcd.setCursor(0,3);  
+        lcd.print(" 3 mm");
+        opt = 2;
+      }
+      if(12 <= counter && counter < 18)
+      {
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Select thickness:");
+        lcd.setCursor(0,1);  
+        lcd.print(" 1 mm");
+        lcd.setCursor(0,2);  
+        lcd.print(" 2 mm");
+        lcd.setCursor(0,3); 
+        lcd.write(0); 
+        lcd.print("3 mm");
+        opt = 3;
+      }
+    }
+    if(menu == 2)
+    {  
+      if(0 <= counter && counter < 9)
+      {
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.write(1);
+        lcd.print("Material: ");
+        lcd.print(materialName);
+        lcd.setCursor(0,1);
+        lcd.write(1);  
+        lcd.print("Thickness: ");
+        lcd.print(thick);
+        lcd.setCursor(0,2); 
+        lcd.write(0); 
+        lcd.print("Continue");
+        lcd.setCursor(0,3);  
+        lcd.print(" Return");
+        opt = 1;
+      }
+      if(9 <= counter && counter < 18)
+      {
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.write(1);
+        lcd.print("Material: ");
+        lcd.print(materialName);
+        lcd.setCursor(0,1);
+        lcd.write(1);  
+        lcd.print("Thickness: ");
+        lcd.print(thick);
+        lcd.setCursor(0,2);  
+        lcd.print(" Continue");
+        lcd.setCursor(0,3);
+        lcd.write(0);  
+        lcd.print("Return");
+        opt = 2;
+      }
+    }
+    if(menu == 3)
+    {  
+      if(0 <= counter && counter < 9)
+      {
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Total sheet count:");
+        lcd.setCursor(0,1);
+        lcd.write(1);  
+        lcd.print("  ");
+        lcd.print(sheetCount);
+        lcd.print("  sheets");
+        lcd.setCursor(0,2);
+        lcd.write(0);  
+        lcd.print("Make another sheet");
+        lcd.setCursor(0,3);  
+        lcd.print(" Return to options");
+        opt = 1;
+      }
+      if(9 <= counter && counter < 18)
+      {
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Total sheet count:");
+        lcd.setCursor(0,1);
+        lcd.write(1);  
+        lcd.print("  ");
+        lcd.print(sheetCount);
+        lcd.print("  sheets");
+        lcd.setCursor(0,2);  
+        lcd.print(" Make another sheet");
+        lcd.setCursor(0,3); 
+        lcd.write(0); 
+        lcd.print("Return to options");
+        opt = 2;
+      }
+    }
+  }
+
+  lastCounter = counter;
 }
 
 void heaters() {
@@ -60,8 +259,8 @@ else{
   lcd.clear();
   lcd.print("Put the sheet");
 }
-int limitStart = digitalRead(limitSwitch[0];
-int limitEnd = digitalRead(limitSwitch[1];
+int limitStart = digitalRead(limitSwitch[0]);
+int limitEnd = digitalRead(limitSwitch[1]);
 if(limitStart == 1){
   digitalWrite(heatRelay , HIGH);
 }
@@ -94,7 +293,7 @@ else{
   lcd.print("Push heaters");
   digitalWrite(buzzer,HIGH);
 }
-
+}
 
 
 
