213908d88ec3a2ae1af8ab6370142a533ade45b3
EianX <30997809+EianX@users.noreply.github.com>
2021-02-16 17:45:59 +0200

    latest version

--- prototype/prototype/prototype.ino
+++ prototype/prototype/prototype.ino
@@ -1,7 +1,7 @@
 /*
  *- Project neame: Vacuum forming machine prototype.
  *- Programmers: Abdelrahman Nasr Maaroof, Mahmoud Ali Abdalmonem, Mahmoud Ibrahim Osman.
- *- Revision history: V3.6 
+ *- Revision history: V4.6 
  *- Description: Manual vacuum forming machine used to shape plastic materials. During the vacuum forming process, a sheet of plastic is heated and then pulled around a single mould using suction.
  *- Development details:
       MCU: Arduino mega, SW: Arduino IDE.
@@ -14,28 +14,27 @@
 #include <max6675.h> //Thermocouple Temperature Sensor K Type with the Maxim MAX6675 K-Thermocouple to digital converter IC library by Adafruit
 #include <Encoder.h> //Encoder library by Paul Stoffregen
 //Pins variables:
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7); //LCD pins (rs, en, d4, d5, d6, d7)
-Encoder myEnc(, ); //Encoder signal pins (interrupt pins for best performance)
-MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO); //Thermocouple Temperature Sensor K Type signal pins (thermoCLK, thermoCS, thermoDO)
-const int encButton = ; //Encoder push button
-const int lamp[2] = { , }; //Index(0): for vacuum lamp, Index(1): for fans lamp
-const int heatRelay = ; //For heaters and its lamp
-const int irPin[4] = {, , , }; //4 IR sensors
-const int buzzer = ;
-const int limitSwitch[3]={, , }; //Index(0,1): start and end for heaters, Index(2): end for platen
-const int fan = ;
-const int vacuum = ; //The vacuum source relay
+LiquidCrystal lcd(29, 31, 37, 39, 41, 43); //LCD pins (rs, en, d4, d5, d6, d7)
+Encoder myEnc(18, 19); //Encoder signal pins (interrupt pins for best performance)
+MAX6675 thermocouple(4, 3, 2); //Thermocouple Temperature Sensor K Type signal pins (thermoCLK, thermoCS, thermoDO)
+const int encButton = 17; //Encoder push button
+const int lamp[2] = {45, 35}; //Index(0): for vacuum lamp, Index(1): for fans lamp
+const int heatRelay = 21; //For heaters and its lamp
+const int buzzer = 11;
+const int limitSwitch[3]={27, 23, 25}; //Index(0,1): start and end for heaters, Index(2): end for platen
+const int fan = 10;
+const int vacuum = 20; //The vacuum source relay
 //Data variables:
 int material = 0; //Acrylic-PVC:0, PP:1
 String materialName = "";
 int thick = 0;
+String thickName = "";
 float temp = 0.0;
 float tempMax = 0.0, tempMin = 0.0; //The temperature range for forming the sheet
 int heatTime = 0; //The time needed for the sheet to be formed
 int heatTimeCount = 0; 
 int sheetCount = 0;
-int vacuumTime = 20; //The vacuum time
-int irRead[4]={, , , }; //The 4 IR sensors reads
+int vacuumTime = 25; //The vacuum time
 //LCD Menues variables:
 long counter = 0; //Encoder position
 int menu = 0;
@@ -43,11 +42,12 @@ int opt = 0;
 long lastCounter = -999;
 bool pushed = true;
 //Material and thickness heat forming data:
-const float tempRange[2][2] = {{140.0, 190.0}, {150.0, 180.0}}; //tempMin = tempRange[material][0], tempMax = tempRange[material][1]
-const int thickTime[2][4]={{0, 30, 60, 90}, {0, 50, 100, 150}}; //heatTime = thickTime[material][thick]
+const float tempRange[2][2] = {{150.0, 190.0}, {150.0, 180.0}}; //tempMin = tempRange[material][0], tempMax = tempRange[material][1]
+const int thickTime[2][4]={{0, 19, 29, 39}, {0, 20, 30, 40}}; //heatTime = thickTime[material][thick]
 //LCD special characters:
-uint8_t arrow[8] = {0x0, 0x04, 0x06, 0x1f, 0x06, 0x04, 0x00, 0x00}; //Arrow char
-uint8_t checked[8] = {0x1F, 0x13, 0x13, 0x13, 0x1B, 0x1B, 0x15, 0x1F}; //Checked char
+uint8_t arrow[8] = {0x00, 0x04, 0x06, 0x1f, 0x06, 0x04, 0x00, 0x00}; //Arrow char
+uint8_t checked[8] = {0x00, 0x10, 0x10, 0x14, 0x12, 0x1F, 0x02, 0x04}; //Checked char
+uint8_t slash[8] = {0x00, 0x10, 0x18, 0x0C, 0x06, 0x03, 0x01, 0x00}; //Backslash char
 
 
 void setup() {
@@ -58,25 +58,25 @@ void setup() {
   pinMode(buzzer, OUTPUT);
   pinMode(fan, OUTPUT);
   pinMode(vacuum, OUTPUT);
-  for(int i = 0; i < 4; i++){
+  for(int i = 0; i < 3; i++){
     if(i < 2)
     {
       pinMode(lamp[i], OUTPUT);
-    }
-    if(i < 3)
-    {
-      pinMode(limitSwitch[i], INPUT);
+      digitalWrite(lamp[i], HIGH);
     }
     
-    pinMode(irPin[i], INPUT);
+    pinMode(limitSwitch[i], INPUT);
   }
-  
+  //Initialize the outputs
+  digitalWrite(heatRelay, LOW);
+  digitalWrite(buzzer, LOW);
+  digitalWrite(fan, LOW);
+  digitalWrite(vacuum, HIGH);
   //Initialize the LCD
   lcd.begin(20, 4);
-  lcd.init();                 //Init the LCD
-  lcd.backlight();            //Activate backlight
-  lcd.createChar(0, arrow);   //Create the arrow symbol
   lcd.createChar(1, checked); //Create the checked symbol
+  lcd.createChar(2, arrow);   //Create the arrow symbol
+  lcd.createChar(3, slash); //Create the checked symbol
   lcd.home();                 //Home the LCD
 
   //Display the welcome message
@@ -87,7 +87,8 @@ void setup() {
   lcd.print("Machine Prototype");
   lcd.setCursor(5, 3);  
   lcd.print("WELCOME :D");
-  delay(2500);
+  delay(3000);
+  lcd.clear();
 }
 
 void loop() {
@@ -109,11 +110,10 @@ void loop() {
     {  
       if(0 <= counter && counter < 6)
       {
-        lcd.clear();
         lcd.setCursor(0,0);
         lcd.print("Select material:");
         lcd.setCursor(0,1);
-        lcd.write(0);  
+        lcd.write(2);  
         lcd.print("PVC");
         lcd.setCursor(0,2);  
         lcd.print(" Acrylic");
@@ -123,13 +123,12 @@ void loop() {
       }
       if(6 <= counter && counter < 12)
       {
-        lcd.clear();
         lcd.setCursor(0,0);
         lcd.print("Select material:");
         lcd.setCursor(0,1);  
         lcd.print(" PVC");
         lcd.setCursor(0,2); 
-        lcd.write(0); 
+        lcd.write(2); 
         lcd.print("Acrylic");
         lcd.setCursor(0,3);  
         lcd.print(" PP");
@@ -137,7 +136,6 @@ void loop() {
       }
       if(12 <= counter && counter < 18)
       {
-        lcd.clear();
         lcd.setCursor(0,0);
         lcd.print("Select material:");
         lcd.setCursor(0,1);  
@@ -145,7 +143,7 @@ void loop() {
         lcd.setCursor(0,2);  
         lcd.print(" Acrylic");
         lcd.setCursor(0,3); 
-        lcd.write(0); 
+        lcd.write(2); 
         lcd.print("PP");
         opt = 3;
       }
@@ -155,44 +153,41 @@ void loop() {
     {  
       if(0 <= counter && counter < 6)
       {
-        lcd.clear();
         lcd.setCursor(0,0);
         lcd.print("Select thickness:");
         lcd.setCursor(0,1);
-        lcd.write(0);  
-        lcd.print("1 mm");
+        lcd.write(2);  
+        lcd.print("300 um");
         lcd.setCursor(0,2);  
-        lcd.print(" 2 mm");
+        lcd.print(" 650 um");
         lcd.setCursor(0,3);  
-        lcd.print(" 3 mm");
+        lcd.print(" 1 mm");
         opt = 1;
       }
       if(6 <= counter && counter < 12)
       {
-        lcd.clear();
         lcd.setCursor(0,0);
         lcd.print("Select thickness:");
         lcd.setCursor(0,1);  
-        lcd.print(" 1 mm");
+        lcd.print(" 300 um");
         lcd.setCursor(0,2); 
-        lcd.write(0); 
-        lcd.print("2 mm");
+        lcd.write(2); 
+        lcd.print("650 um");
         lcd.setCursor(0,3);  
-        lcd.print(" 3 mm");
+        lcd.print(" 1 mm");
         opt = 2;
       }
       if(12 <= counter && counter < 18)
       {
-        lcd.clear();
         lcd.setCursor(0,0);
         lcd.print("Select thickness:");
         lcd.setCursor(0,1);  
-        lcd.print(" 1 mm");
+        lcd.print(" 300 um");
         lcd.setCursor(0,2);  
-        lcd.print(" 2 mm");
+        lcd.print(" 650 um");
         lcd.setCursor(0,3); 
-        lcd.write(0); 
-        lcd.print("3 mm");
+        lcd.write(2); 
+        lcd.print("1 mm");
         opt = 3;
       }
     }
@@ -201,7 +196,6 @@ void loop() {
     {  
       if(0 <= counter && counter < 9)
       {
-        lcd.clear();
         lcd.setCursor(0,0);
         lcd.write(1);
         lcd.print("Material: ");
@@ -209,10 +203,9 @@ void loop() {
         lcd.setCursor(0,1);
         lcd.write(1);  
         lcd.print("Thickness: ");
-        lcd.print(thick);
-        lcd.print(" mm");
+        lcd.print(thickName);
         lcd.setCursor(0,2); 
-        lcd.write(0); 
+        lcd.write(2); 
         lcd.print("Continue");
         lcd.setCursor(0,3);  
         lcd.print(" Return");
@@ -220,7 +213,6 @@ void loop() {
       }
       if(9 <= counter && counter < 18)
       {
-        lcd.clear();
         lcd.setCursor(0,0);
         lcd.write(1);
         lcd.print("Material: ");
@@ -228,12 +220,11 @@ void loop() {
         lcd.setCursor(0,1);
         lcd.write(1);  
         lcd.print("Thickness: ");
-        lcd.print(thick);
-        lcd.print(" mm");
+        lcd.print(thickName);
         lcd.setCursor(0,2);  
         lcd.print(" Continue");
         lcd.setCursor(0,3);
-        lcd.write(0);  
+        lcd.write(2);  
         lcd.print("Return");
         opt = 2;
       }
@@ -243,16 +234,15 @@ void loop() {
     {  
       if(0 <= counter && counter < 9)
       {
-        lcd.clear();
         lcd.setCursor(0,0);
         lcd.print("Total sheet count:");
         lcd.setCursor(0,1);
         lcd.write(1);  
         lcd.print("  ");
         lcd.print(sheetCount);
-        lcd.print("  sheets");
+        lcd.print("  sheets      ");
         lcd.setCursor(0,2);
-        lcd.write(0);  
+        lcd.write(2);  
         lcd.print("Make another sheet");
         lcd.setCursor(0,3);  
         lcd.print(" Return to options");
@@ -260,18 +250,17 @@ void loop() {
       }
       if(9 <= counter && counter < 18)
       {
-        lcd.clear();
         lcd.setCursor(0,0);
         lcd.print("Total sheet count:");
         lcd.setCursor(0,1);
         lcd.write(1);  
         lcd.print("  ");
         lcd.print(sheetCount);
-        lcd.print("  sheets");
+        lcd.print("  sheets      ");
         lcd.setCursor(0,2);  
         lcd.print(" Make another sheet");
         lcd.setCursor(0,3); 
-        lcd.write(0); 
+        lcd.write(2); 
         lcd.print("Return to options");
         opt = 2;
       }
@@ -280,9 +269,11 @@ void loop() {
 
   lastCounter = counter;
   //The menus different options actions
-  if(digitalRead(encButton)){
+  if(!digitalRead(encButton)){
+    delay(500);
     pushed = true;
     myEnc.write(0);
+    lcd.clear();
     //1st menu save the material number and name
     if(menu == 0)
     {
@@ -305,19 +296,31 @@ void loop() {
       }
     }
     //2nd menu save the thickness
-    if(menu == 1)
+    else if(menu == 1)
     {
       menu = 2;
       thick = opt;
+      if(opt == 1)
+      {
+        thickName = "300 um";
+      }
+      if(opt == 2)
+      {
+        thickName = "650 um";
+      }
+      if(opt == 3)
+      {
+        thickName = "1 mm";
+      }
     }
     //3rd and 4th menus preform the vacuum forming process 
-    if(menu == 2 || menu == 3)
+    else if(menu == 2 || menu == 3)
     {
       if(opt == 1)
       {
         menu = 3;
         heaters();
-        vacuum();
+        Vacuum();
       }
       if(opt == 2)
       {
@@ -330,45 +333,55 @@ void loop() {
 
 void heaters() {
   //The heaters section:
+  tempMin = tempRange[material][0];
+  tempMax = tempRange[material][1];
+  heatTime = thickTime[material][thick];
   //Display place the sheet instruction
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Push back the Heater");
   lcd.setCursor(0,1);
   lcd.print("Then place the Sheet");
-  lcd.setCursor(6,3);
+  lcd.setCursor(6,2);
   lcd.print("Please..");
-  //Read the 4 IR sensors till the sheet is placed right
-  do {
-    for(int i = 0; i < 4; i++){
-      irRead[i] = digitalRead(irPin[i]);
-    }
-  }while(irRead[0] == 0 || irRead[1] == 0 || irRead[2] == 0 || irRead[3] == 0);
+  lcd.setCursor(0,3);
+  lcd.write(2);
+  lcd.print("Continue");
+  //Read the limit switch till the heaters are pushed
+  while(digitalRead(encButton) || !digitalRead(limitSwitch[1]));
+  //Wait till the heaters warm up  
+  digitalWrite(heatRelay , HIGH);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Heater is warming up");
+  lcd.setCursor(3,1);
+  lcd.print("Please Wait...");
+  lcd.setCursor(0,3);
+  lcd.print("Temp: ");
+  lcd.print(tempMin);
+  lcd.write(3);
+  do{
+    temp = thermocouple.readCelsius();
+    lcd.setCursor(13,3);
+    lcd.print(temp);
+    delay(500);
+    lcd.print("  ");
+  }while(temp < tempMin);
   //Display pull the heaters instruction
   lcd.clear();
   lcd.setCursor(2,1);
   lcd.print("Pull the Heaters");
   lcd.setCursor(2,2);
   lcd.print("forward Please..");
-  //Read the start limit switch for the heaters till it reaches
-  do{
-    int limitStart = digitalRead(limitSwitch[0]);
-  }while(limitStart == 0);
-  //Wait till the heaters warm up  
-  digitalWrite(heatRelay , HIGH);
-  lcd.clear();
-  lcd.setCursor(0,1);
-  lcd.print("Heater is warming up");
-  lcd.setCursor(3,2);
-  lcd.print("Please Wait...");
-
-  tempMin = tempRange[material][0];
-  tempMax = tempRange[material][1];
-  heatTime = thickTime[material][thick];
-  
-  do{
-    temp = thermocouple.readCelsius();
-  }while(temp < tempMin);
+  //Turn on the buzzer and read the start limit switch for the heaters till it reaches
+  do{ 
+    digitalWrite(buzzer, HIGH);
+    delay(350);
+    digitalWrite(buzzer, LOW);
+    delay(350);
+  }while(!digitalRead(limitSwitch[0]));
+  //Turn off buzzer after completing
+  digitalWrite(buzzer, 0);
   //The forming time countdown
   lcd.clear();
   lcd.setCursor(0,0);
@@ -377,16 +390,16 @@ void heaters() {
   lcd.print("has been reached");
   lcd.setCursor(3,3);
   lcd.print("Countdown : ");
-  lcd.print(heatTime)
+  lcd.print(heatTime);
   
-  for(heatTimeCount = 0; heatTimeCount <= heatTime * 4; heatTimeCount++){
+  for(heatTimeCount = 0; heatTimeCount <= heatTime * 2; heatTimeCount++){
     temp = thermocouple.readCelsius();
-    if(heatTimeCount % 4 == 0)
+    delay(500);
+    if(heatTimeCount % 2 == 0)
     {
-      lcd.setCursor(12,3);
-      lcd.print("     ");
-      lcd.setCursor(12,3);
-      lcd.print(heatTime - (heatTimeCount / 4));
+      lcd.setCursor(15,3);
+      lcd.print(heatTime - (heatTimeCount / 2));
+      lcd.print("  ");
     }
     if(temp > tempMax)
     {
@@ -396,35 +409,33 @@ void heaters() {
     {
       digitalWrite(heatRelay , HIGH);
     }
-    delay(250);
   }
-    //Turn off heaters after completing
-    digitalWrite(heatRelay , LOW);
-    lcd.clear();
-    lcd.setCursor(2,0);
-    lcd.print("Heating  process");
-    lcd.setCursor(1,1);
-    lcd.print("has been completed");
-    lcd.setCursor(0,2);
-    lcd.print("Push back the Heater");
-    lcd.setCursor(3,3);
-    lcd.print("again Please..");
-  //Read the end limit switch for the heaters till it reaches
+  //Turn off heaters after completing
+  digitalWrite(heatRelay , LOW);
+  lcd.clear();
+  lcd.setCursor(2,0);
+  lcd.print("Heating  process");
+  lcd.setCursor(1,1);
+  lcd.print("has been completed");
+  lcd.setCursor(0,2);
+  lcd.print("Push back the Heater");
+  lcd.setCursor(3,3);
+  lcd.print("again Please..");
+  //Turn on the buzzer and read the end limit switch for the heaters till it reaches
   do{ 
-    int limitEnd = digitalRead(limitSwitch[1]);
-    for(int i = 50; i < 256; i++){
-      analogWrite(buzzer, i);
-      delay(10);
-    }
-  }while(limitEnd == 0);
+    digitalWrite(buzzer, HIGH);
+    delay(350);
+    digitalWrite(buzzer, LOW);
+    delay(350);
+  }while(!digitalRead(limitSwitch[1]));
   //Turn off buzzer after completing
-  analogWrite(buzzer, 0);
+  digitalWrite(buzzer, 0);
 }
 
 
 
   
-void vacuum() {
+void Vacuum() {
   //The vacuum section:
   //Display pull up the platen instruction
   lcd.clear();
@@ -433,54 +444,71 @@ void vacuum() {
   lcd.setCursor(6,2);  
   lcd.print ("Please..");
   //Read the end limit switch for the platen till it reaches       
-  do {
-    int limitSwitchState = digitalRead(limitSwitch[2]);
-    digitalWrite(lamp[0], (millis() / 1000) % 2);
-  }while(limitSwitchState == 0);  
+  while(!digitalRead(limitSwitch[2]));  
   //Trun on the vacuum and its lamp after completing
   digitalWrite(vacuum, LOW);
-  digitalWrite(lamp[0], HIGH); 
+  digitalWrite(lamp[0], LOW);
+  lcd.clear();
+  lcd.setCursor(3,0);
+  lcd.print("Vacuum Process");
+  lcd.setCursor(3,1);
+  lcd.print("in progress ..");
+  lcd.setCursor(3,2);
+  lcd.print("Countdown : ");
+  lcd.begin(20, 4);
   //Vacuum time countdown   
-  for(int i = vacuumTime; i > 0; i--){
-    lcd.clear();
-    lcd.setCursor(3,0);
-    lcd.print("Vacuum Process");
-    lcd.setCursor(3,1);
-    lcd.print("in progress ..");
-    lcd.setCursor(3,2);
-    lcd.print("Countdown : ");
-    lcd.print(i); 
+  for(int i = vacuumTime; i >= 0; i--){
     //Turn on fans and its lamp
-    if(i == 8)
+    if(i == 10)
     {
       digitalWrite(fan, HIGH);
       digitalWrite(lamp[1], LOW);
+      lcd.begin(20, 4);
     }
     //Turn off vacuum source and its lamp
     if(i == 5)
     {
       digitalWrite(vacuum, HIGH);
-      digitalWrite(lamp[0], LOW); 
+      digitalWrite(lamp[0], HIGH);
+      lcd.begin(20, 4); 
     }
-    //Turn off fans and its lamp
-    if(i == 0)
-    {
-      digitalWrite(fan, LOW);
-      digitalWrite(lamp[1], HIGH);
-    }
-   delay(1000);
- }
+    lcd.begin(20, 4);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("   Vacuum Process   ");
+    lcd.setCursor(0,1);
+    lcd.print("   in progress ..   ");
+    lcd.setCursor(0,2);
+    lcd.print("   Countdown :      "); 
+    lcd.setCursor(15,2);
+    lcd.print(i);
+    lcd.setCursor(0,3);
+    lcd.print("                    "); 
+    delay(1000);
+  }
+  delay(500);
   //Display take out the sheet instruction
+  lcd.begin(20, 4);
+  lcd.createChar(1, checked); 
+  lcd.createChar(2, arrow);   
+  lcd.createChar(3, slash); 
   lcd.clear();
   lcd.setCursor(3,1);  
   lcd.print ("Sheet is Ready");
   lcd.setCursor(1,2);
   lcd.print("You can take it ..");
+  lcd.setCursor(0,3);
+  lcd.write(2);
+  lcd.print("Continue");
   sheetCount++;
   //Wait till the sheet is taken out  
-  do{
-    for(int i = 0; i < 4; i++){
-      irRead[i] = digitalRead(irPin[i]);
-    }
-  }while(irRead[0] == 1 && irRead[1] == 1 && irRead[2] == 1 && irRead[3] == 1);
+  while(digitalRead(encButton));
+  //Turn off fans and its lamp
+  digitalWrite(fan, LOW);
+  digitalWrite(lamp[1], HIGH);
+  lcd.begin(20, 4);
+  lcd.createChar(1, checked); 
+  lcd.createChar(2, arrow);   
+  lcd.createChar(3, slash); 
+  delay(500);
 }
